be99d5118bf1fa04e1223bd76510477c883c0f66
Denis Laliberté <denis@tp1.ca>
2014-12-27 12:00:20 -0500

    refactor to remove a parameter and use a closure

--- src/RomanNumber.js
+++ src/RomanNumber.js
@@ -3,15 +3,15 @@ function RomanNumber(Arabic) {
     'value':  [ 100, 50,  40,   10,  9,     5,  4,     1],
     'symbol': [ 'C', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ]
   }
-  return asdf(Arabic,association,0);
+  function closure(Arabic,index) {
+    if(Arabic >= association.value[index] ) {
+      return association.symbol[index] + closure(Arabic-association.value[index], index);
+    }
+    if(index >= association.value.length) {
+      return "";
+    }
+    return closure(Arabic,index+1);
+  }
+  return closure(Arabic,0);
 }
 
-function asdf(Arabic,association,index) {
-  if(Arabic >= association.value[index] ) {
-    return association.symbol[index] + asdf(Arabic-association.value[index], association, index);
-  }
-  if(index >= association.value.length) {
-    return "";
-  }
-  return asdf(Arabic,association,index+1);
-}
