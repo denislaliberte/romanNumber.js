895cd909a6f833705a5a657609bf5d7b1e4765c1
Denis Laliberté <denis@tp1.ca>
2014-12-27 01:16:55 -0500

    refactor

--- src/RomanNumber.js
+++ src/RomanNumber.js
@@ -2,10 +2,10 @@ function RomanNumber(Arabic) {
   if(Arabic >=10){
     return "X" + RomanNumber(Arabic -10);
   }
-  else if(Arabic >= 5){
+  if(Arabic >= 5){
     return "V" + RomanNumber(Arabic -5);
   }
-  else if(Arabic >=1 && Arabic <=3){
+  if(Arabic >=1 && Arabic <=3){
     return "I" + RomanNumber(Arabic -1);
   }
   return ""
