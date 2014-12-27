7c67a2bb5bd893848f4a4a8ed29bedcf44366282
Denis Laliberté <denis@tp1.ca>
2014-12-27 00:58:17 -0500

    add case II

--- spec/RomanNumberSpec.js
+++ spec/RomanNumberSpec.js
@@ -3,8 +3,8 @@ describe("RomanNumber", function() {
     expect(RomanNumber(1)).toEqual('I');
     }
   );
-//  it("should return II for 2", function() {
-//    expect(RomanNumber(2)).toEqual('II');
-//    }
-//  );
+  it("should return II for 2", function() {
+    expect(RomanNumber(2)).toEqual('II');
+    }
+  );
 });
