c5d9099ad37c46bd63b7c3d5ff4cd26b3652b86c
Denis Laliberté <denis@tp1.ca>
2014-12-27 11:47:55 -0500

    cleanup

--- src/RomanNumber.js
+++ src/RomanNumber.js
@@ -1,5 +1,4 @@
 function RomanNumber(Arabic) {
-  //return old(Arabic);
   association = {
     'value':  [ 100, 50,  40,   10,  9,     5,  4,     1],
     'symbol': [ 'C', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ]
@@ -11,11 +10,8 @@ function asdf(Arabic,association,index) {
   if(Arabic >= association.value[index] ) {
     return association.symbol[index] + asdf(Arabic-association.value[index], association, index);
   }
-  if(index >=7) {
+  if(index >= association.value.length) {
     return "";
   }
   return asdf(Arabic,association,index+1);
 }
-
-
-
